023caedbe45e4453cd83ee79b2621815786950b4
Ma. Isabel Veloso <veloso.maisabel@gmail.com>
2024-09-01 22:38:05 +0800

    Add files via upload

---
+++ Chapter 1/SleepIn.java
@@ -0,0 +1,23 @@
+public class SleepIn {
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday && !vacation) {
+            return true;
+        } else if (!weekday && vacation) {
+            return true;
+        } else if (weekday && !vacation) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        SleepIn sleepIn = new SleepIn();
+        
+        System.out.println(sleepIn.sleepIn(false, false)); 
+        System.out.println(sleepIn.sleepIn(true, false));
+        System.out.println(sleepIn.sleepIn(false, true));
+        System.out.println(sleepIn.sleepIn(true, true));
+    }
+}
