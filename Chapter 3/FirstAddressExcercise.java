0eb1ebe1fcdd16d540bf74b375b802d34c186407
Ma. Isabel Veloso <veloso.maisabel@gmail.com>
2024-09-16 23:42:17 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExcercise {
+
+    public static void main(String[] args) {
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        String company = "Apple";
+        String companyAddress = "One Apple Park Way\nCupertino, CA 95014";
+
+        System.out.println("Address: " + companyAddress);
+        System.out.println("Company: " + company);
+    }
+
+}
