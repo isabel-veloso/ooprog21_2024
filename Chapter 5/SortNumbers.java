737290adfb4085cd2bb0df0255b517dc04f043c9
Ma. Isabel Veloso <veloso.maisabel@gmail.com>
2024-10-24 00:23:29 +0800

    SortNumbers.java

--- Chapter 5/SortNumbers.java
+++ Chapter 5/SortNumbers.java
@@ -5,25 +5,22 @@ public class SortNumbers {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Enter first number:");
+    System.out.println("Enter three numbers:");
     int num1 = scanner.nextInt();
-    System.out.println("Enter second number:");
     int num2 = scanner.nextInt();
-    System.out.println("Enter third number:");
     int num3 = scanner.nextInt();
 
     if (num1 <= num2 && num2 <= num3) {
-      System.out.println("Numbers in ascending order: " + num1 + " " + num2 + " " + num3);
+      System.out.println("Your number are already in ascending order. \n" +  "" + num1 + " " + num2 + " " + num3);
     } else if (num1 >= num2 && num2 >= num3) {
-      System.out.println("Numbers in ascending order: " + num3 + " " + num2 + " " + num1);
-    } else if (num1 <= num2 && num2 >= num3) {
-      if (num1 <= num3) {
-        System.out.println("Numbers in ascending order: " + num1 + " " + num3 + " " + num2);
-      } else {
-        System.out.println("Numbers in ascending order: " + num3 + " " + num1 + " " + num2);
-      }
+      System.out.println("Your numbers are in descending order: \n" + " "+ num1 + " " + num2 + " " + num3);
+      System.out.println("Here's the ascending order: \n" + " "+ num3 + " " + num2 + " " + num1);
     } else {
-      System.out.println("Numbers in ascending order: " + num2 + " " + num3 + " " + num1);
+      int min = Math.min(Math.min(num1, num2), num3);
+      int max = Math.max(Math.max(num1, num2), num3);
+      int mid = (num1 + num2 + num3) - min - max;
+      System.out.println("Your numbers are not in order! \nNumbers in descending order: \n" + max + " " + mid + " " + min);
+      System.out.println("Numbers in ascending order: \n" + min + " " + mid + " " + max);
     }
 
     scanner.close();
